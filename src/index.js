2482f9f54a5a488a4e362bdd1752c42797c6de52
JarrodNB <jnb.cb38@gmail.com>
2020-07-08 17:05:28 -0400

    Added axios for ajax. Made calls to API to retreive list, and create baby.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3000/api/v1';
+
+// debugging - remove
+axios.interceptors.response.use(response => {
+  console.log(response);
+  return response;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
 
 ReactDOM.render(
   <App />,
