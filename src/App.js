a17ad832c4091bd063c309f8c19fc77baffc42b6
JarrodNB <jnb.cb38@gmail.com>
2020-07-09 11:37:53 -0400

    Added react-router-dom. Added list_id paramter to url. Added error message for unique names. Project should now meet minimum requirements.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,46 @@ import React, { Component } from 'react';
 import './App.css';
 import BabyList from './BabyList/BabyList';
 import axios from 'axios';
+import { withRouter } from "react-router";
 
 class App extends Component {
 
   state = {
+    id: null,
     publicId: null,
     babies: null,
-    newBabyName: ""
+    newBabyName: "",
+    errorMessage: null
   }
 
   componentDidMount() {
-    axios.get('/lists')
+    let queryId = new URLSearchParams(this.props.location.search).get("list_id");
+    if (queryId){
+      axios.get('/lists/' + queryId)
       .then(response => {
         this.setState({
           id: response.data.id,
           publicId: response.data.public_id,
           babies: response.data.babies
         });
+     })
+      .catch(response => {
+        //to do
+      });
+    } else {
+      axios.get('/lists')
+      .then(response => {
+        this.setState({
+          id: response.data.id,
+          publicId: response.data.public_id,
+          babies: response.data.babies
+        });
+        this.props.history.push(`?list_id=${this.state.publicId}`);
     })
       .catch(response => {
         //to do
       });
+    }
   }
 
   addBabyHandler = (event) => {
@@ -36,13 +55,13 @@ class App extends Component {
         this.updateCurrentList();
       })
       .catch(error => {
-        //to do
+        this.setState({errorMessage: error.response.data.name[0]});
         console.log(error.response.data.name[0]);
       });
   }
 
   updateCurrentList = () => {
-    axios.get('/lists/' + this.state.id)
+    axios.get('/lists/' + this.state.publicId)
       .then(response => {
         this.setState({
           id: response.data.id,
@@ -56,10 +75,17 @@ class App extends Component {
   }
 
   babyNameHandler = (event) => {
-    this.setState({newBabyName: event.target.value});
+    this.setState({
+      newBabyName: event.target.value,
+      errorMessage: null
+    });
   }
 
   render() {
+    let error = null;
+    if (this.state.errorMessage){
+      error = <h5 style={{color: 'red'}}>{this.state.errorMessage}</h5>
+    }
     return (
       <div className="App">
         <h1>Jarrod's Baby Name List</h1>
@@ -72,6 +98,7 @@ class App extends Component {
           onClick={this.addBabyHandler}>
           Submit Baby Name
         </button>
+        {error}
         <BabyList 
           publicId={this.state.publicId} 
           babies={this.state.babies}
@@ -81,4 +108,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
