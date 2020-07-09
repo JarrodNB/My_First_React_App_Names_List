fe70ee7dddb942a136eb254e7e9495143f322dd2
JarrodNB <jnb.cb38@gmail.com>
2020-07-09 15:29:29 -0400

    Added on click for baby component to change crossed out status. Added red x background image.

--- src/Baby/Baby.js
+++ src/Baby/Baby.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './Baby.css';
 
 const Baby = (props) => {
+  let babyClasses = "Baby";
+  if (props.crossedOut){
+    babyClasses += " cross";
+  }
 
   return (
-    <div className="Baby">
+    <div className={babyClasses} onClick={props.clicked}>
       <h5>{props.name}</h5>
     </div>
   );
